a0ed057f0d24654a9340da9c1f3d98b22724527d
fuadaeff <fabdulla057@gmail.com>
2024-12-03 20:27:39 +0400

    Add files via upload

---
+++ workingWithLists/workingWithLists/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace workingWithLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            Console.WriteLine("Eded daxil edin:");
+            int a = int.Parse(Console.ReadLine());
+            while (a != -1)
+            {
+                numbers.Add(a);
+                Console.WriteLine("Eded daxil edin:");
+                a = int.Parse(Console.ReadLine());
+            }
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+    }
+}
