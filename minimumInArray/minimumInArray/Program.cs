341f2413bce1653228555f2931d53d1793046835
fuadaeff <fabdulla057@gmail.com>
2024-12-03 20:35:49 +0400

    Add files via upload

---
+++ minimumInArray/minimumInArray/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace minimumInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = { 123, 324, 456, 234, 4 };
+            int minimum = number[0];
+            foreach (var minimal in number)
+            {
+                if(minimal < minimum)
+                {
+                    minimum = minimal;
+                }
+
+            }
+            Console.WriteLine(minimum);           
+        }
+    }
+}
