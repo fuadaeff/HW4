ca427adf7d2272dddd362f95df2486d2f9a68e5d
fuadaeff <fabdulla057@gmail.com>
2024-12-03 20:28:13 +0400

    Add files via upload

---
+++ workingWithArrays/workingWithArrays/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace workingWithArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[5];
+            string name;
+            int i = 5;
+            int index = 0;
+            while (i > 0)
+            {
+                Console.WriteLine("Ad daxil edin:");
+                name = Console.ReadLine();
+                names[index] = name;
+                index = index + 1;
+                i = i - 1;
+
+            }
+            foreach (string output in names) {
+                Console.WriteLine(output);
+            
+            }
+
+        }
+    }
+}
