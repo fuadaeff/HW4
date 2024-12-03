14d030a8408ff87b7cde529aa54014072acfa10b
fuadaeff <fabdulla057@gmail.com>
2024-12-03 20:57:49 +0400

    Add files via upload

---
+++ listOfTheBooks/listOfTheBooks/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace listOfTheBooks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> books = new List<string>();
+            int i = 5;
+            while (i > 0) { 
+                i = i - 1;
+                Console.WriteLine("Enter a book name:");
+                string input = Console.ReadLine();
+                books.Add(input);
+            }
+            foreach (var book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+}
