930eb36c4766500edf85211dcbb7ccd0cd5ca9d6
fuadaeff <fabdulla057@gmail.com>
2024-12-03 21:10:05 +0400

    Add files via upload

---
+++ reverseArray/reverseArray/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace reverseArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 23, 93, 141, 58 };
+            int[] reversed = new int[numbers.Length];
+            int i = numbers.Length - 1;
+            int a = 0;
+            while (i >= 0)
+            {
+                reversed[a] = numbers[i];
+                i = i - 1;
+                a = a + 1;
+            }
+            foreach (var output in reversed)
+            {
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
